7aa952838999f5ba5aedd1d3da21b5f3543d6dd4
= <=>
2024-11-30 09:31:45 +0700

    fix schema

--- server/src/utility/mailer.ts
+++ server/src/utility/mailer.ts
@@ -1,8 +1,8 @@
 import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com',
-    port: 465,
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
     secure: true,
     auth: {
         user: process.env.EMAIL,
